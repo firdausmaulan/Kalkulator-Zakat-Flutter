bf32f81cced43c6792fe2d6f1416b67942987fa7
Maulana Firdaus <maulana.firdaus@advotics.com>
2020-06-09 10:07:41 +0700

    v3

---
+++ lib/features/exit_dialog.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:kalkulatorzakat/utils/constants.dart' as Constants;
+import 'package:kalkulatorzakat/utils/images.dart' as Images;
+
+void showExitDialog(context) {
+  double width = MediaQuery.of(context).size.width;
+  double buttonWidth = width / 3;
+  showModalBottomSheet<dynamic>(
+    isScrollControlled: true,
+    backgroundColor: Colors.white,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.only(topLeft: Radius.circular(25), topRight: Radius.circular(25))
+    ),
+    context: context,
+    builder: (context) => Wrap(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            children: <Widget>[
+              Image.asset(Images.ic_question),
+              SizedBox(height: 10),
+              Text("Apakah Anda yakin untuk keluar?"),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(
+                    width: buttonWidth,
+                    child: OutlineButton(
+                        onPressed: () async {
+                          Navigator.pop(context);
+                        },
+                        borderSide: BorderSide(color: Colors.green),
+                        child: Text(
+                          "Tidak".toUpperCase(),
+                          style: TextStyle(
+                              color: Colors.green,
+                              fontWeight: FontWeight.bold),
+                        )),
+                  ),
+                  SizedBox(width: 10),
+                  SizedBox(
+                      width: buttonWidth,
+                      child: RaisedButton(
+                          onPressed: () async {
+                            SystemNavigator.pop();
+                          },
+                          color: Constants.PRIMARY_COLOR,
+                          child: Text(
+                            "Ya".toUpperCase(),
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          )))
+                ],
+              )
+            ],
+          ),
+        ),
+      ],
+    ));
+}
